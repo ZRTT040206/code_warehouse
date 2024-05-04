889a633e84874c8a3b54d932e23728c235039054
Jerry <2963025245@qq.com>
2024-05-04 16:13:49 +0800

    2024_5_4_fifth

---
+++ acwing/16_replace_spaces.cpp
@@ -0,0 +1,16 @@
+// acwing 16_replace_spaces
+/*class Solution {
+public:
+    string replaceSpaces(string &str) {
+        string res;
+        for (int i = 0; i < str.size(); i++) { 
+            if (str[i] != ' ') {
+                res += str[i];
+            } else {
+                res += "%20";
+            }
+        }
+        return res;
+    }
+};
+*/
