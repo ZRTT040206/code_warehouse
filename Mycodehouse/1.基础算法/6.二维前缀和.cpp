955f8a3b75e03308a6ab47e1b05ab6a9a8727f84
Jerry <2963025245@qq.com>
2025-01-26 14:37:49 +0800

    2025/1/26/1

--- Mycodehouse/1.基础算法/6.二维前缀和.cpp
+++ Mycodehouse/1.基础算法/6.二维前缀和.cpp
@@ -1,30 +1,31 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-const int N = 1e6+10;
-int q[N];
-int temp[N];
-void add(int n)
+const int N = 1e3+10;
+// vector<vector<int>> temp,q;
+int q[N][N],temp[N][N];
+void add(int x1,int x2,int y1,int y2)
 {
-    temp[0] = q[0];
-    for(int i=1;i<=n;i++)
-    {
-        temp[i] = q[i]+temp[i-1];
-    }
+    cout<<temp[x2][y2]-temp[x1-1][y2]-temp[x2][y1-1]+temp[x1-1][y1-1]<<endl;
 }
 int main()
 {
-    int n=0,k=0;
-    cin>>n>>k;
-    q[0]=0;
+    int n,p,k=0;
+    scanf("%d%d%d",&n,&p,&k);
+    //temp.resize(n,vector<int>(p,0));
     for(int i=1;i<=n;i++)
     {
-        scanf("%d",&q[i]);
+        for(int j=1;j<=p;j++)
+        {
+            scanf("%d",&q[i][j]);
+            temp[i][j] = q[i][j]+temp[i-1][j]+temp[i][j-1]-temp[i-1][j-1];
+        }
     }
-    add(n);
-    for(int s=1;s<=k;s++)
+    int x1,x2,y1,y2;
+    for(int i=1;i<=k;i++)
     {
-        int i,j;
-        scanf("%d%d",&i,&j);
-        cout<<temp[j]-temp[i-1]<<endl;
+        scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+        add(x1,x2,y1,y2);
     }
+    return 0;
 }
